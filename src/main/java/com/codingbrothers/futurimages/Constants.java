ea39d3f98e69574c18783f38bed1b0fe3ae355f5
premysl-kaloc <premysl-kaloc@server.fake>
2014-11-12 00:23:48 +0100

    endpoints-skeleton

---
+++ src/main/java/com/codingbrothers/futurimages/Constants.java
@@ -0,0 +1,10 @@
+package com.codingbrothers.futurimages;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming your API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "881176597931-pi8v9l72t68c6s9lpm3mnhmv1227hog1.apps.googleusercontent.com";
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
