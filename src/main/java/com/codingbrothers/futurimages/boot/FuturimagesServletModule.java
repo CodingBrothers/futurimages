06ce09868b04a4401d4cda778bf396d4478d1b3b
dkaloc <dkaloc@dkaloc-probook>
2014-11-12 23:17:06 +0100

    Google Cloud Endpoints already have support for Guice. Making use of it!

--- src/main/java/com/codingbrothers/futurimages/boot/FuturimagesServletModule.java
+++ src/main/java/com/codingbrothers/futurimages/boot/FuturimagesServletModule.java
@@ -6,17 +6,13 @@ import javax.inject.Singleton;
 
 import com.codingbrothers.futurimages.FuturimagesAPI;
 import com.google.api.server.spi.guice.GuiceSystemServiceServletModule;
-import com.google.inject.servlet.GuiceFilter;
 import com.googlecode.objectify.ObjectifyFilter;
 
 public class FuturimagesServletModule extends GuiceSystemServiceServletModule {
 
 	@Override
 	protected void configureServlets() {
-		bind(GuiceFilter.class).in(Singleton.class);
 		bind(ObjectifyFilter.class).in(Singleton.class);
-
-		filter("/*").through(GuiceFilter.class);
 		filter("/*").through(ObjectifyFilter.class);
 
 		serveGuiceSystemServiceServlet("/*", Collections.singletonList(FuturimagesAPI.class));
