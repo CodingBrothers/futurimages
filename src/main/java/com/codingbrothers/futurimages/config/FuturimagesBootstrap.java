1ac634c1df9b98655ecc37426ee39e80d7091e0d
premysl-kaloc <premysl-kaloc@server.fake>
2014-11-12 20:26:05 +0100

    FuturimagesBootstrap forgot to implement ServletContextListener

--- src/main/java/com/codingbrothers/futurimages/config/FuturimagesBootstrap.java
+++ src/main/java/com/codingbrothers/futurimages/config/FuturimagesBootstrap.java
@@ -2,16 +2,23 @@ package com.codingbrothers.futurimages.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 import com.google.inject.Guice;
 
-public class FuturimagesBootstrap {
+public class FuturimagesBootstrap implements ServletContextListener {
 
+	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		final ServletContext servletContext = servletContextEvent.getServletContext();
 
 		Guice.createInjector(new FuturimagesGuiceModule());
 
 //		servletContext.setAttribute(INJECTOR_NAME, injector);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		
 	}
 }
