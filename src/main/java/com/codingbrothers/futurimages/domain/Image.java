6a58f506469a3a6c0f4f0f733c93f0594ed8a9f1
dkaloc <dkaloc@dkaloc-probook>
2014-11-16 21:52:48 +0100

    Create the Image entity

--- src/main/java/com/codingbrothers/futurimages/domain/Image.java
+++ src/main/java/com/codingbrothers/futurimages/domain/Image.java
@@ -1,10 +1,14 @@
 package com.codingbrothers.futurimages.domain;
 
+import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Unindex;
 
 @Entity
+@Unindex
+//@Cache(expirationSeconds=1)
 public class Image {
 
-	@Id private long id;
+	@Id private String id;
 }
