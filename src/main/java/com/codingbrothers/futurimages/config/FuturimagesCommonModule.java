fe0ad9c99db47499dcddb208b0eba29bf4978269
Dusan Kaloc <dusan.kaloc@gmail.com>
2014-11-24 09:40:25 +0100

    Bind User and the default TaskQueue

--- src/main/java/com/codingbrothers/futurimages/config/FuturimagesCommonModule.java
+++ src/main/java/com/codingbrothers/futurimages/config/FuturimagesCommonModule.java
@@ -1,19 +1,22 @@
 package com.codingbrothers.futurimages.config;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
 import com.codingbrothers.futurimages.domain.Image;
 import com.codingbrothers.futurimages.service.Futurimages;
+import com.codingbrothers.futurimages.service.impl.DeferredImageDataUploader;
 import com.codingbrothers.futurimages.service.impl.FuturimagesImpl;
+import com.codingbrothers.futurimages.service.impl.ImageDataUploader;
 import com.codingbrothers.futurimages.util.RequestContext;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.users.User;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.servlet.RequestScoped;
-import com.googlecode.objectify.Objectify;
-import com.googlecode.objectify.ObjectifyService;
 
 public class FuturimagesCommonModule extends AbstractModule {
 
@@ -24,6 +27,8 @@ public class FuturimagesCommonModule extends AbstractModule {
 		bind(FuturimagesObjectifyFactory.class).asEagerSingleton();
 
 		bind(Futurimages.class).to(FuturimagesImpl.class).asEagerSingleton();
+
+		bind(com.codingbrothers.futurimages.domain.User.class);
 
 		bind(Image.class);
 
@@ -39,10 +44,13 @@ public class FuturimagesCommonModule extends AbstractModule {
 				return (User) request.getAttribute(LOGGED_IN_USER_REQ_ATTR_NAME);
 			}
 		}).in(RequestScoped.class);
+
+		bind(ImageDataUploader.class).to(DeferredImageDataUploader.class);
 	}
 
 	@Provides
-	Objectify provideObjectify() {
-		return ObjectifyService.ofy();
+	@Named("ImageDataUploader")
+	Queue provideQueue() {
+		return QueueFactory.getDefaultQueue();
 	}
 }
