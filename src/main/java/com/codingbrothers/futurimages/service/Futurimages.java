c695fa16d5b977d382d99a4308e2d6b62b8f9fdf
Dusan Kaloc <dusan.kaloc@gmail.com>
2014-11-24 09:39:47 +0100

    Add service methods

--- src/main/java/com/codingbrothers/futurimages/service/Futurimages.java
+++ src/main/java/com/codingbrothers/futurimages/service/Futurimages.java
@@ -1,6 +1,14 @@
 package com.codingbrothers.futurimages.service;
 
+import java.util.Date;
+
+import com.codingbrothers.futurimages.domain.Image;
+
 public interface Futurimages {
 
-	
+	Image createImage(Image image, com.google.appengine.api.images.Image imageData);
+
+	Iterable<Image> getPublicImages(Date createdAfter, Date createdBefore, boolean asc);
+
+	Iterable<Image> getUserImages(String userId, Date createdAfter, Date createdBefore, boolean asc);
 }
