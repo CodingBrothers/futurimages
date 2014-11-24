4ff279450ae16b724e8a8b33159664718bd6cb19
premysl-kaloc <premysl-kaloc@server.fake>
2014-11-24 18:26:27 +0100

    Fix ObjectifySessionClearingFilter

--- src/main/java/com/codingbrothers/futurimages/util/ObjectifySessionClearingFilter.java
+++ src/main/java/com/codingbrothers/futurimages/util/ObjectifySessionClearingFilter.java
@@ -2,26 +2,17 @@ package com.codingbrothers.futurimages.util;
 
 import java.io.IOException;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import com.google.inject.Provider;
-import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.util.AbstractFilter;
 
 @Singleton
 public class ObjectifySessionClearingFilter extends AbstractFilter {
-
-	private final Provider<Objectify> objectifyProvider;
-
-	@Inject
-	public ObjectifySessionClearingFilter(Provider<Objectify> objectifyProvider) {
-		this.objectifyProvider = objectifyProvider;
-	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
@@ -29,7 +20,7 @@ public class ObjectifySessionClearingFilter extends AbstractFilter {
 		try {
 			chain.doFilter(request, response);
 		} finally {
-			objectifyProvider.get().clear();
+			ObjectifyService.ofy().clear();
 		}
 	}
 }
